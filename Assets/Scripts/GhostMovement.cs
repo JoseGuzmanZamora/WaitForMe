c224eef6c01a7a4809caae415e39fac6ae1af144
JoseGuzmanZamora <joseguzmanzamora@gmail.com>
2022-10-12 20:15:37 -0600

    Ghost movement logic

---
+++ Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    public GameObject objective;
+    public int movementSpeed = 6;
+    public bool facesRight = true;
+    private Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody> ();
+    }
+
+    private void FixedUpdate() {
+        var target = objective.transform.position;
+        var currentPosition = transform.position;
+
+        var xDifference = target.x - currentPosition.x;
+        var zDifference = target.z - currentPosition.z;
+        OrientationProcess(xDifference);
+
+        var newPosition = new Vector3(xDifference, transform.position.y, zDifference).normalized * (movementSpeed * Time.deltaTime);
+        rb.MovePosition(transform.position + newPosition);
+    }
+
+    public void OrientationProcess(float xDifference)
+    {
+        // Move the sprite
+        if (xDifference > 0)
+        {
+            if (facesRight)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+        }
+        else if (xDifference < 0)
+        {
+            if (facesRight)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+    }
+}
